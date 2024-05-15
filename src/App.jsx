62ecbc6bfeeb5d0dbdf01e9038b77c2023b43fbd
Acens <acensverse@gmail.com>
2024-05-15 19:52:35 +0530

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,36 @@ import './App.css'
 import { FaHotel } from "react-icons/fa6";
 import { FaTrainSubway } from "react-icons/fa6";
 import { FaCarRear } from "react-icons/fa6";
-import { PiAirplaneTiltFill } from "react-icons/pi";
+import { GiCommercialAirplane } from "react-icons/gi";
+import { CgProfile } from "react-icons/cg";
+import { HiSwitchHorizontal } from "react-icons/hi";
 
 function App() {
 
   return (
     <>
       <nav>
-        <img src="/images/logo.png" alt="logo" className='logo'/>
-        <ul className='trips'>
-          <li className=''> <PiAirplaneTiltFill /> Flights </li>
-          <li> <FaHotel /> Hotels </li>
-          <li> <FaTrainSubway /> Trains </li>
-          <li> <FaCarRear /> Cabs </li>
-        </ul>
-        <ul className='accinfo'>
-          <li> My Trips </li>
-          <li> Login / SignUp </li>
-        </ul>
+        <div className="icon">
+          <img src="/images/logo3.png" alt="logo" className='logo' />
+        </div>
+        
+        <div className="midPart">
+          <ul className='trips'>
+            <li> <GiCommercialAirplane className='mr-2' /> Flights </li>
+            <li> <FaHotel className='mr-2'/> Hotels </li>
+            <li> <FaTrainSubway className='mr-2'/> Trains </li>
+            <li> <FaCarRear className='mr-2' /> Cabs </li>
+          </ul>
+        </div>
+
+        <div className="accPart">
+          <ul className='accinfo'>
+            <li> My Trips </li>
+            <li className='border border-blue-500 rounded-xl bg-blue-500'> <CgProfile  className='mr-2'/> LOGIN / SIGNUP </li>
+          </ul>
+        </div>
       </nav>
+
       <section>
         <div className="booking">
           <form action="" id='station'>
@@ -34,28 +45,32 @@ function App() {
             <form action="" id='place'>
 
               <div className="relative">
-                <label className=' absolute left-10 bg-white text-xl text-gray-500 font-medium'> From  </label>
-                <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' class='border border-gray-500 rounded-xl p-8 m-3' />
+                <label className=' absolute left-10 bg-white text-l text-gray-500 font-medium'> From  </label>
+                <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' class='border border-gray-500 rounded-xl p-8 mt-3 mx-1 text-black font-bold' />
               </div>
 
               <div className="relative">
-                <label className='absolute left-10 bg-white text-xl text-gray-500 font-medium'> To </label>
-                <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' class='border border-gray-500 rounded-xl p-8 m-3' />
+               <HiSwitchHorizontal className='absolute text-blue-500 text-4xl bg-white border border-blue-500 rounded-full z-10 p-2' />
               </div>
 
               <div className="relative">
-                <label className='absolute left-10 bg-white text-xl text-gray-500 font-medium'> Departure </label>
-                <input type="date" name="placeName" id="placeName" placeholder='Departure' class='border border-gray-500 rounded-xl p-8 m-3' />
+                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> To </label>
+                <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' class='border border-gray-500 rounded-xl p-8 mt-3 mx-1 text-black font-bold' />
+              </div>
+
+              <div className="relative">
+                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> Departure </label>
+                <input type="date" name="placeName" id="placeName" placeholder='Departure' class='border border-gray-500 rounded-xl p-8 mt-3 mx-1' />
               </div>
               
               <div className="relative">
-                <label className='absolute left-10 bg-white text-xl text-gray-500 font-medium'> Return </label>
-                <input type="date" name="placeName" id="placeName" placeholder='Return' class='border border-gray-500 rounded-xl p-8 m-3' />
+                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> Return </label>
+                <input type="date" name="placeName" id="placeName" placeholder='Return' class='border border-gray-500 rounded-xl p-8 mt-3 mx-1' />
               </div>
               
               <div className="relative">
-                <label className='absolute left-10 bg-white text-xl text-gray-500 font-medium'> Travellers & Class </label>
-                <input type="text" name="placeName" id="placeName" placeholder='Travellers & Class' class='border border-gray-500 rounded-xl p-8 m-3' />
+                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> Travellers & Class </label>
+                <input type="text" name="placeName" id="placeName" placeholder='Travellers & Class' class='border border-gray-500 rounded-xl p-8 mt-3 mx-1' />
               </div>
             </form>
 
@@ -67,7 +82,7 @@ function App() {
               <label class='border border-gray-500 p-4 rounded-xl'> <input type="radio" name="specialfare" id="specialFare" class='m-2 p-4'/> Docter and Nurses </label>
             </div>
             <div className="search">
-              <button class=' bg-gradient-to-r from-blue-200 to-blue-800 rounded-full p-4 px-12'> SEARCH </button>
+              <button class=' bg-gradient-to-r from-blue-400 to-blue-800 rounded-full p-4 px-12'> SEARCH </button>
             </div>
           </div>
         </div>
