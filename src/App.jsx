602d09803a73997d9e8a09fc44411d5ddb4de77e
Acens <acensverse@gmail.com>
2024-05-16 21:22:47 +0530

    ui update 2

--- src/App.jsx
+++ src/App.jsx
@@ -27,14 +27,15 @@ function App() {
   },[])
 
   return (
-    <div className='app' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${bgImage})`}}>
-      <nav>
+    <div className='app' style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${bgImage})` }}>
+      
+      <nav className='relative md:static flex items-center justify-between md:justify-around'>
         <div className="icon">
-          <img src="/images/logo3.png" alt="logo" className='logo' />
+          <img src="/images/logo3.png" alt="logo" className='w-16 md:w-36 ml-4 md:ml-auto' />
         </div>
         
-        <div className="midPart">
-          <ul className='trips'>
+        <div className="midPart, hidden ml-60 md:flex">
+          <ul className='trips flex items-center justify-center list-none p-4 text-white text-xl'>
             <li> <GiCommercialAirplane className='mr-2' /> Flights </li>
             <li> <FaHotel className='mr-2'/> Hotels </li>
             <li> <FaTrainSubway className='mr-2'/> Trains </li>
@@ -43,91 +44,105 @@ function App() {
         </div>
 
         <div className="accPart">
-          <ul className='accinfo'>
+          <ul className='text-white text-sm md:text-xl p-4 flex items-center justify-center'>
             <li> My Trips </li>
-            <li className='border border-blue-500 rounded-xl bg-blue-500'> <CgProfile  className='mr-2'/> LOGIN / SIGNUP </li>
+            <li className='border border-blue-500 rounded-xl bg-blue-500 text-lg'> <CgProfile className='mr-2'/> LOGIN / SIGNUP </li>
           </ul>
         </div>
       </nav>
 
-      <section>
-        <div className="booking">
-          <form action="" id='station'>
-            <label class='m-4 text-gray-500 font-bold'><input type="radio" id='selectST' name='options' /> One Way </label>
-            <label class='m-4 text-gray-500 font-bold'><input type="radio" id='selectST' name='options' /> Round Trip </label>
-            <label class= 'm-4 text-gray-500 font-bold'><input type="radio" id='selectST' name='options' /> Multi City </label>
-            <p class='ml-20 mr-4 text-gray-500'> Book International and Domestic Flight </p>
-          </form>
+      {/* Mobile view Port Trip Section */}
 
-          <div className="specialfarebooking">
-            <form action="" id='place'>
+      <div className="md:hidden mt-4">
+        <ul className='flex items-center justify-evenly list-none text-black'>
+          <li className='flex flex-col border border-white rounded-xl bg-white '> <GiCommercialAirplane className='m-2' /> Flights </li>
+          <li className='flex flex-col border-white rounded-xl bg-white'> <FaHotel className='m-2'/> Hotels </li>
+          <li className='flex flex-col border-white rounded-xl bg-white'> <FaTrainSubway className='m-2'/> Trains </li>
+          <li className='flex flex-col border-white rounded-xl bg-white px-5'> <FaCarRear className='m-2' /> Cabs </li>
+        </ul>
+      </div>
 
-              <div className="relative">
-                <label className=' absolute left-10 bg-white text-l text-gray-500 font-medium'> From  </label>
-                <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' class='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1 text-black font-bold' />
-              </div>
+      {/* --------------------------------------------------------------------------------------------- */}
 
-              {/* Switch button */}
+      <section className=' static md:relative md:flex flex-col items-center justify-evenly md:justify-center mt-5 md:mt-20 p-4 md:p-0'>
 
-              <div className="relative flex justify-center items-center align-middle">
-                <div className=" absolute z-10 mt-2.5">
-                <HiSwitchHorizontal className='text-blue-500 text-4xl bg-white border border-white rounded-full p-2' id='switch' />
-                </div>
-              </div>
+        <form action="" id='station' className='flex items-center justify-around bg-white z-10 rounded-xl md:p-4'>
+          <label className='p-4 text-gray-500 font-bold'><input type="radio" id='selectST' name='options' /> One Way </label>
+          <label className='p-4 text-gray-500 font-bold'><input type="radio" id='selectST' name='options' /> Round Trip </label>
+          <label className='p-4 text-gray-500 font-bold'><input type="radio" id='selectST' name='options' /> Multi City </label>
+          <p className='ml-20 mr-4 text-gray-500 hidden md:block'> Book International and Domestic Flight </p>
+        </form>
 
-              <div className="relative">
-                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> To </label>
-                <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' class='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1 text-black font-bold' />
-              </div>
+        {/* Travels Section and Station Options */}
 
-              <div className="relative">
-                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> Departure </label>
-                <input type="date" name="placeName" id="placeName" placeholder='Departure' class='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1' />
-              </div>
-              
-              <div className="relative">
-                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> Return </label>
-                <input type="date" name="placeName" id="placeName" placeholder='Return' class='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1' />
-              </div>
-              
-              <div className="relative">
-                <label className='absolute left-10 bg-white text-l text-gray-500 font-medium'> Travellers & Class </label>
-                <input type="text" name="placeName" id="placeName" placeholder='Travellers & Class' class='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1' />
-              </div>
-            </form>
+        <form action="" id='place' className='static md:absolute md:flex md:flex-row md:items-center md:justify-center md:pt-20 md:pb-40 md:px-4 flex-col items-center justify-evenly bg-white rounded-xl md:mt-88 p-5 mt-2'>
 
-            <div className="special" >
-              <div className="m-2">
-                <h1 className='font-bold'> Special Fares </h1>
-                <h1 className='bg-gradient-to-r from-indigo-400 to-indigo-800 p-2 rounded-md text-sm text-white font-bold'> EXTRA SAVINGS </h1>
-              </div>
+          <div className="relative">
+            <label className='absolute left-10 px-1 bg-white text-l text-gray-500 font-medium'> From  </label>
+            <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' className='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1 text-black font-bold' />
+          </div>
 
-              <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">
-                <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" class='mr-2' /> Regular <p className='text-xs text-gray-500 font-medium ml-10'> Regular Fares </p> </label>
-              </div>
-              
-              <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">               
-                <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" class='mr-2' /> Student <p className='text-xs text-gray-500 font-medium ml-10'> Extra Discounts </p> </label>
-              </div>
-              
-              <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">               
-                <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" class='m-2 p-4'/> Senior Citizen <p className='text-xs text-gray-500 font-medium ml-10'> Exclusive Discounts </p> </label>              
-              </div>
-              
-              <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">               
-                <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" class='m-2 p-4'/> Armed Forces <p className='text-xs text-gray-500 font-medium ml-10'> Exclusive Discounts </p> </label>          
-              </div>
-              
-              <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">              
-                <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" class='m-2 p-4'/> Docter and Nurses <p className='text-xs text-gray-500 font-medium ml-1'> Exclusive Discounts </p> </label>             
-              </div>
-            </div>
+          {/* Switch button */}
 
-            <div className="search">
-              <button class=' bg-gradient-to-r from-blue-400 to-blue-800 rounded-full p-4 px-12'> SEARCH </button>
+          <div className="relative flex justify-center items-center align-middle">
+            <div className=" absolute z-10 mt-2.5">
+            <HiSwitchHorizontal className='text-blue-500 text-4xl bg-white border border-white rounded-full p-2' id='switch' />
             </div>
           </div>
+
+          <div className="relative">
+            <label className='absolute left-10 px-1 bg-white text-l text-gray-500 font-medium'> To </label>
+            <input type="text" name="placeName" id="placeName" placeholder='Enter city or airport' className='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1 text-black font-bold' />
+          </div>
+
+          <div className="relative">
+            <label className='absolute left-10 px-1 bg-white text-l text-gray-500 font-medium'> Departure </label>
+            <input type="date" name="placeName" id="placeName" placeholder='Departure' className='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1' />
+          </div>
+          
+          <div className="relative">
+            <label className='absolute left-10 px-1 bg-white text-l text-gray-500 font-medium'> Return </label>
+            <input type="date" name="placeName" id="placeName" placeholder='Return' className='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1' />
+          </div>
+          
+          <div className="relative">
+            <label className='absolute left-10 px-1 bg-white text-l text-gray-500 font-medium'> Travellers & className </label>
+            <input type="list" name="placeName" id="placeName" placeholder='1 Adult Economy' className='border-2 border-gray-300 rounded-xl p-8 mt-3 mx-1' />
+          </div>
+        </form>
+
+        {/* Special Fares section */}
+
+        <div className="special hidden md:flex items-center justify-evenly absolute z-1 mt-120" >
+          <div className="m-2">
+            <h1 className='font-bold'> Special Fares </h1>
+            <h1 className='bg-gradient-to-r from-indigo-400 to-indigo-800 p-2 rounded-md text-sm text-white font-bold'> EXTRA SAVINGS </h1>
+          </div>
+
+          <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">
+            <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" className='mr-2' /> Regular <p className='text-xs text-gray-500 font-medium ml-10'> Regular Fares </p> </label>
+          </div>
+          
+          <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">               
+            <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" className='mr-2' /> Student <p className='text-xs text-gray-500 font-medium ml-10'> Extra Discounts </p> </label>
+          </div>
+          
+          <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">               
+            <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" className='m-2 p-4'/> Senior Citizen <p className='text-xs text-gray-500 font-medium ml-10'> Exclusive Discounts </p> </label>              
+          </div>
+          
+          <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">               
+            <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" className='m-2 p-4'/> Armed Forces <p className='text-xs text-gray-500 font-medium ml-10'> Exclusive Discounts </p> </label>          
+          </div>
+          
+          <div className="border-2 border-gray-300 rounded-xl pt-2 pb-2 pr-2 m-2">              
+            <label className='font-bold text-gray-800'> <input type="radio" name="specialfare" id="specialFare" className='m-2 p-4'/> Docter and Nurses <p className='text-xs text-gray-500 font-medium ml-1'> Exclusive Discounts </p> </label>             
+          </div>
         </div>
+
+          <div className="search">
+            <button id='search'className=' bg-gradient-to-r from-blue-400 to-blue-800 rounded-full p-4 px-12'> SEARCH </button>
+          </div>
       </section>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '100': '25rem',
+        '88': '22rem',
+        '120': '30rem',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
