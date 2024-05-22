73f40df9a1226b4913622de4c18e4d7eec93b743
Acens <acensverse@gmail.com>
2024-05-23 00:30:30 +0530

    login v3

--- src/components/Login/Signup.jsx
+++ src/components/Login/Signup.jsx
@@ -6,7 +6,7 @@ import { IoMdClose } from "react-icons/io";
 function Signup() {
   return (
     <div>
-      <div className="md:fixed inset-0 bg-black opacity-50 z-20"></div>
+      <div className="md:fixed inset-0 md:bg-black opacity-50 z-20"></div>
 
       <div className='md:absolute md:flex items-center justify-start md:p-20 md:ml-165 h-screen z-20'>
 
@@ -14,9 +14,13 @@ function Signup() {
           <IoMdClose className='cancel' />
         </button> </Link>
 
-        <div className=' bg-white border rounded-xl flex flex-col items-center justify-evenly p-8 md:py-10'>
+        <div className=' bg-white border rounded-xl flex flex-col items-center justify-start md:justify-evenly md:p-8 md:py-10 h-full md:h-auto '>
+
+          <Link to='/' className='md:hidden'> <button>
+            <IoMdClose className='cancel' />
+          </button> </Link>
         
-          <h1 className='bg-blue-600 px-8 py-2 text-lg font-bold border rounded-full bg-gradient-to-r from-purple-400 to to-blue-800 text-white mb-8'> Sky Wings Account </h1>
+          <h1 className='bg-blue-600 px-8 py-2 text-lg font-bold border rounded-full bg-gradient-to-r from-purple-400 to to-blue-800 text-white mb-8 mt-8 md:mt-auto'> Sky Wings Account </h1>
 
           
           <div className='flex flex-col items-center justify-evenly'>
@@ -26,14 +30,14 @@ function Signup() {
             </div>
             <span className='mb-2 text-sm font-medium mr-72' > Email Address </span>
 
-            <div className=''>
-              <input type="email" name="email" id="emailId" className='border border-blue-300 rounded-md p-2 px-4 mr-4' placeholder='Enter your email id' />
+            <div className='flex flex-col w-full md:flex md:flex-row'>
+              <input type="email" name="email" id="emailId" className='border border-blue-300 rounded-md p-2 px-4 mr-4 w-full mb-4 md:mb-0' placeholder='Enter your email id'/>
               <button className='py-2 px-8 bg-blue-800 text-white font-bold border rounded-md'> Continue </button>
             </div>
 
             <div className='flex flex-col items-center justify-center'>
               <span className='text-xs font-semibold text-red-500 m-4'> Forgot Login Id ? </span>
-              <p className='uppercase text-gray-400 font-semibold text-xs mt-8'> Or use your business account with </p>
+              <p className='uppercase text-gray-400 font-semibold text-xs md:mt-8'> Or use your business account with </p>
 
               <img src="images/google.png" alt="" className='w-8 border border-gray-300 rounded-full p-2 m-4' />
               <span className='text-xs text-gray-400'> By proceeding you agree to Sky Wings's <Link className='text-red-500'> T&Cs </Link> and <Link className='text-red-500'> Privacy </Link> </span>
