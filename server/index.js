fb7c75abb33f83c318c91c062336e13f38ac802f
Acens <acensverse@gmail.com>
2024-06-04 12:21:28 +0530

    ui v6

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/User.js');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://Ace:Ace123@cluster0.y5dyrlw.mongodb.net/user", {
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB', err);
+});
+
+app.post('/Signup', (req, res) => {
+  const {email} = req.body
+  UserModel.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.json({ error: err.message }));
+});
+
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
