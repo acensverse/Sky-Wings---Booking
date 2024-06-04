652fa6ce4d03e6e804b99aed0358466e68872cda
Acens <acensverse@gmail.com>
2024-06-04 17:02:53 +0530

    ui v6.5

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const UserModel = require('./models/User.js');
-
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = ['https://sky-wings-booking.vercel.app']; // Add the domain where your React app is hosted
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    // Check if the origin is allowed
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `This site is not allowed to access the resource`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 mongoose.connect("mongodb+srv://Ace:Ace123@cluster0.y5dyrlw.mongodb.net/user", {
 })
